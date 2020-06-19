34cec83700ff1c91c95b7e2f983f10b83a452331
mirjSolution <ricojohndatoon23@gmail.com>
2020-06-19 04:53:17 -0400

    deploy

---
+++ backend/public/build/precache-manifest.542464155d65193ebc0f94b0e31eb78c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3e99ba57b6ccb9f2ffad91373bdfa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b25bd91191f2968d87",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "609b2c1018d52e94617e",
+    "url": "/static/css/main.8126fc30.chunk.css"
+  },
+  {
+    "revision": "64b25bd91191f2968d87",
+    "url": "/static/js/2.54698a32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54698a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609b2c1018d52e94617e",
+    "url": "/static/js/main.861f6152.chunk.js"
+  },
+  {
+    "revision": "b60d3c3616117ebfce12",
+    "url": "/static/js/runtime-main.20abbc0e.js"
+  },
+  {
+    "revision": "45adbe8a98013ff8852033b3f9996756",
+    "url": "/static/media/band.45adbe8a.jpg"
+  }
+]);
